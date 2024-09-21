b1416c892907c60a2f49d79e73fb6a7fed671df0
Praneeth <ppraneeth861@gmail.com>
2024-09-21 21:38:52 +0530

    Reminder created

--- src/Reminder.java
+++ src/Reminder.java
@@ -1,2 +1,18 @@
-public class Reminder {
+class reminder{
+    int reminderId;
+    String duedate;
+    String message;
+
+    reminder(int reminderId, String duedate, String message){
+        this.reminderId = reminderId;
+        this.duedate = duedate;
+        this.message = message;
+    }
+
+    void getReminderDetails(){
+        System.out.println("Reminder ID: " + reminderId);
+        System.out.println("Duedate: " + duedate);
+        System.out.println("Message: " + message);
+    }
 }
+
